960ff75364066e2adc452785ead7cd95da12d056
이홍석 <lhs0806@gmail.com>
2022-05-10 15:53:17 +0900

    05/10

---
+++ lec_02.js
@@ -0,0 +1,44 @@
+// readline 사용
+var readline = require('readline'); 
+var r = readline.createInterface({ input:process.stdin, output:process.stdout }); 
+r.question("종료를 하려면 Return키를 누르시요\n", function(answer) { 
+    console.log("프로그램 종료!", answer); 
+    r.close(); // 반드시 close()를 해줘야 합니다. 
+    server.close();
+    process.exit();
+});
+
+const express = require("express");
+const logger = require("morgan")
+const app = express();
+const port = 3000
+
+// 미들웨어 예제
+const mw1 = (req, res, next) => {
+    console.log("mw1");
+    next();
+}
+
+const mw2 = (req, res, next) => {
+    console.log("mw2");
+    next();
+}
+
+const mw = (req, res, next) => {
+    throw Error('미들웨어 에러!')
+}
+const errorMw = (err, req, res, next) => {
+    console.log('여기서 에러를 캣치 합니다 ',err.message) 
+    next()      
+}
+
+
+// EXPRESS 서버구동
+const server = app.listen(port, () => console.log(`Example app listening on port ${port}`) )
+app.use(mw1)
+app.use(mw2)
+app.use(logger('dev'))
+
+app.use(mw)
+app.use(errorMw)
+
---
+++ lec_03.js
@@ -0,0 +1,64 @@
+// readline 사용
+var readline = require('readline'); 
+var r = readline.createInterface({ input:process.stdin, output:process.stdout }); 
+r.question("종료를 하려면 Return키를 누르시요\n", function(answer) { 
+    console.log("프로그램 종료!", answer); 
+    r.close(); // 반드시 close()를 해줘야 합니다. 
+    server.close();
+    process.exit();
+});
+
+
+/*
+// 서버자원 호출 
+
+app.get // 자원을 조회
+app.post // 자원을 생성
+app.delete  // 자원을 삭제
+app.put // 자원을 갱신
+app.patch // 자원을 갱신
+
+HTTP 상태코드
+
+• 1xx: 아직 처리중
+• 2xx: 자, 여기있어!
+• 3xx: 잘 가~ (redirect)
+• 4xx: 니가 문제임
+• 5xx: 내가 문제임
+
+• 200: 성공(success), GET, PUT
+• 201: 작성됨(created), POST
+• 204: 내용 없음 (No Conent), DELETE
+• 400: 잘못된 요청 (Bad Request)
+• 401: 권한 없음 (Unauthorized)
+• 404: 찾을 수 없음 (Not found)
+• 409: 충돌 (Conflict)
+• 500: 서버 에러 (Interel server error)
+
+*/
+
+const express = require("express");
+const app = express();
+const port = 3000
+
+
+// EXPRESS 서버구동
+const server = app.listen(port, () => console.log(`Example app listening on port ${port}`) )
+
+// get 요청 라우팅 예제
+app.get("/root", function(req, res){
+    console.log("called root")
+    res.send("Hello world!")
+})
+// curl localhost:3000/subs -v 404에러 확인
+app.get("/error", function(req, res){
+    console.log("called error")
+    throw Error(' 여기서 임의로 에러를 발생시켜 보았습니다..!')
+    res.send("Hello world! - sub")
+})
+
+const users = {name: 'Alice'}
+
+app.get("/users", function(req, res){
+    res.json(users)
+})
