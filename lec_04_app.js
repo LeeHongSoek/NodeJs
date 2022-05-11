2530722e78d1e15a5ea6d4134eacdb87aae78933
이홍석(Lenovo노트북) <lhs0806@gmail.com>
2022-05-11 14:32:46 +0900

    4강 - TDD 이해 및 환경 구축

--- lec_04_app.js
+++ lec_04_app.js
@@ -1,13 +1,10 @@
 // https://tacademy.skplanet.com/live/player/onlineLectureDetail.action?seq=134
  
 const express = require("express");
- 
 const app = express();
-//const port = 3000
-
 
 // EXPRESS 서버구동
-//const server = app.listen(port, () => console.log(`Example app listening on port ${port}`) )
+//const server = app.listen(3000, () => console.log(`Example app listening on port ${port}`) )
 
 const users = [{name: 'Alice'}]
 
