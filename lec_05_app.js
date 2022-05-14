20c7288280b5cab07642417aeae1647cf47d0170
이홍석(서울데스크탑) <lhs0806@gmail.com>
2022-05-14 22:27:10 +0900

    5강 - TDD로 API 서버 개발 - 소스정리

--- lec_05_app.js
+++ lec_05_app.js
@@ -1,7 +1,6 @@
 const express = require("express");
 const app = express();
 
-
 // 사용자 정보가 배열 형태로 구성
 const users = [{id:1, name: 'Alice'},
                {id:2, name: 'Bek'},
@@ -9,17 +8,18 @@ const users = [{id:1, name: 'Alice'},
               ] 
 
 app.get("/", (req, res) => { res.send('hello world!')}) 
+
 app.get("/users", (req, res) => { 
     req.query.limit = req.query.limit || 10 //  10, "10", "ten" 
     const limit = parseInt(req.query.limit, 10 )
     
-    if(Number.isNaN(limit))
+    if(Number.isNaN(limit)) // 정수가 아닌경우
     {
-        res.status(400).end()
+        res.status(400).end() // 400에러를 리턴
     }
     else 
     {
-        res.json(users.slice(0,limit)) 
+        res.json(users.slice(0,limit))  // user를 리턴
     }    
 })
 
