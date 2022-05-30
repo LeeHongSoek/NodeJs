d5277d852f6e42822eeb3c379c71dad05525fefc
이홍석(GMail) <105229755+LeeHongSoek@users.noreply.github.com>
2022-05-31 00:19:55 +0900

    getter setter 적용

--- MySqlApp/router_data/common.js
+++ MySqlApp/router_data/common.js
@@ -1,61 +1,113 @@
-const page_info = {
+const pageInfo = {
     
-    totalrow: -1, // 총 레코드 수
-
-    rowperpage: 10, // 페이지당 레코드수
-    pageperblock: 10, // 블럭당 페이지 수
-    
-    currpage: 1, // 현재 페이지
-    currblock: 1, // 현재 블럭
-
-    // mysql 의 limt 문의 첫번째 아규먼트가 들어가 수치
-    getlimitfrom: function() {        
-        this.currpage = this.getcurrpage()
-
-        if ((this.currpage-1) * this.rowperpage > this.totalrow) {
-            this.currpage = parseInt(this.totalrow / this.rowperpage) 
-            this.currpage += (this.totalrow % this.rowperpage > 0) ? 1 : 0  // 나머지가 있으면 +1 
-        }
-        return (this.currpage - 1) * this.rowperpage 
+    // 총 레코드 수 (디폴트 없음 - 반드시 먼저 지정)
+    _totalRow: -1, 
+    get totalRow() {
+        return this._totalRow
     },
-    // 현재 페이지를  범위 보정후 리턴한다.
-    getcurrpage: function() { 
-        if (this.currpage < 1) this.currpage = 1
-
-        var totalpage = parseInt(this.totalrow / this.rowperpage)
-        totalpage += (this.totalrow % this.rowperpage > 0) ? 1 : 0 
-        if (totalpage < this.currpage){
-            this.currpage = totalpage
-        }
-        return this.currpage
+    set totalRow(value) {
+        this._totalRow = (value < 1) ? 1 : value // 반드시 1 보다 커야함.        
     },
-    // 총레코드수를 기반으로 총페이지 수를 구한다.
-    gettotalpages: function() {
-        var totalpage = parseInt(this.totalrow / this.rowperpage)
-        totalpage += (this.totalrow % this.rowperpage > 0) ? 1 : 0 
 
-        return totalpage
+    // 페이지당 레코드수
+    _rowPerPage: 10, 
+    get rowPerPage() {
+        return this._rowPerPage
+    },
+    set rowPerPage(value) {        
+        this._rowPerPage = (value < 1) ? 1 : value // 반드시 1 보다 커야함.        
     },    
-    // 현재 블럭 번호를 알아 낸다
-    getcurrblock: function() {
-        var currpage = this.getcurrpage() 
-        
-        this.currblock = parseInt(currpage / this.pageperblock)
-        this.currblock += (currpage % this.pageperblock > 0) ? 1 : 0 
 
-        return this.currblock
+    // 블럭당 페이지 수
+    _pagePerBlock: 10, 
+    get pagePerBlock() {
+        return this._pagePerBlock        
     },
+    set pagePerBlock(value) {
+        this._pagePerBlock = (value < 1) ? 1 : value // 반드시 1 보다 커야함.        
+    },
+
+    // 총레코드수를 기반으로 총페이지 수를 구한다.
+    _totalPages: 0,
+    get totalPages() {
+        var totalPage = parseInt(this._totalRow / this._rowPerPage)
+        totalPage += (this._totalRow % this._rowPerPage > 0) ? 1 : 0 
+
+        return totalPage
+    },
+    set totalPages(value) {
+        this._totalPages = value
+    },
+    // 총레코드수를 기반으로 총블럭 수를 구한다.
+    _totalBlock: 0,
+    get totalBlock() {
+        var totalBlock = parseInt(this._totalRow / (this._rowPerPage * this._pagePerBlock))
+        totalBlock += (this._totalRow % (this._rowPerPage * this._pagePerBlock) > 0) ? 1 : 0 
+
+        return totalBlock
+    },
+    set totalBlock(value) {
+        this._totalBlock = value
+    },
+     
+    // 현재 페이지
+    _currPage: 1,     
+    get currPage() { // 현재 페이지를  범위 보정후 리턴한다.
+        if (this._currPage < 1) this._currPage = 1
+
+        var totalPage = parseInt(this._totalRow / this._rowPerPage)
+        totalPage += (this._totalRow % this._rowPerPage > 0) ? 1 : 0 
+        if (totalPage < this._currPage){
+            this._currPage = totalPage
+        }
+        return this._currPage
+    },
+    set currPage(value) {
+        this._currPage = value
+    },
+
+    // 현재 블럭 번호
+    _currBlock: 0,
+    get currBlock() {
+        this._currBlock = parseInt(this._currPage / this._pagePerBlock)
+        this._currBlock += (this._currPage % this._pagePerBlock > 0) ? 1 : 0 
+
+        return this._currBlock
+    },
+    set currBlock(value) {
+        this._currBlock = value
+    },
+    
+    // mysql 의 limt 문의 첫번째 아규먼트가 들어가 수치
+    _limitFrom: -1,    
+    get limitFrom() {       
+        if ((this._currPage-1) * this._rowPerPage > this._totalRow) {
+            this._currPage = parseInt(this._totalRow / this._rowPerPage) 
+            this._currPage += (this._totalRow % this._rowPerPage > 0) ? 1 : 0  // 나머지가 있으면 +1 
+        }
+        return (this._currPage - 1) * this._rowPerPage 
+    },
+    set limitFrom(value) {
+        this._limitFrom = value
+    },
+
+    _limitTo: -1,
+    get limitTo() {
+        return this._rowPerPage
+    },
+
+    
     // 현재 블럭의 페이지 수를 10개 혹은 이하로 리스팅한다.
-    getcurrpagelist: function() {
+    getcurrPagelist: function() {
         var arrypage = []
 
-        for (var i=1; i<=this.pageperblock; i++) {
-            var cntpage = ((this.getcurrblock()-1) * this.pageperblock) + i
-            if (cntpage <= this.gettotalpages())
+        for (var i=1; i<=this._pagePerBlock; i++) {
+            var cntpage = ((this._currBlock-1) * this._pagePerBlock) + i
+            if (cntpage <= this.totalPages)
                 arrypage.push(cntpage)
         }
         return arrypage;
     }
 }
 
-module.exports = page_info
+module.exports = pageInfo
--- MySqlApp/router_data/test.js
+++ MySqlApp/router_data/test.js
@@ -1,25 +1,23 @@
-const page_info = require('./common')
+const pageInfo = require('./common')
 
-page_info.totalrow = 1220
-page_info.rowperpage = 10
-page_info.currpage = 121
-page_info.currblock = 1
+pageInfo.totalRow = 120
+pageInfo.rowPerPage = 10
+pageInfo.pagePerBlock = 5
+pageInfo.currPage = 9
+pageInfo.currBlock = 1
 
-console.log('page_info.totalrow -> ',page_info.totalrow)
+console.log('pageInfo.totalRow : ',pageInfo.totalRow)
+console.log('pageInfo.totalPages : ',pageInfo.totalPages)
+console.log('pageInfo.totalBlock : ',pageInfo.totalBlock)
+console.log('pageInfo.currPage : ',pageInfo.currPage)
+console.log('pageInfo.currBlock : ',pageInfo.currBlock)
+console.log('pageInfo.limtFrom : ',pageInfo.limitFrom)
+console.log('pageInfo.limtTo : ',pageInfo.limitTo)
 
-console.log('page_info.limtfrom() -> ',page_info.getlimitfrom())
-console.log('page_info.limtto -> ',page_info.rowperpage)
-
-console.log('page_info.gettotalpages() -> ',page_info.gettotalpages())
-console.log('page_info.getcurrpage() -> ',page_info.getcurrpage())
-console.log('page_info.getcurrblock() -> ',page_info.getcurrblock())
-
-var paglst = page_info.getcurrpagelist()
-console.log('page_info.currpagelist() -> ',paglst)
-
-var pageView = ''
-for(idx in paglst){
-    pageView += ', '
-    pageView += (page_info.getcurrpage()===paglst[idx]) ? '['+paglst[idx]+']' : paglst[idx] 
+var pagLst = pageInfo.getcurrPagelist()
+var strPge = ''
+for(i in pagLst){
+    strPge += ', '
+    strPge += (pageInfo.currPage===pagLst[i]) ? '['+pagLst[i]+']' : pagLst[i] 
 }
-console.log(pageView.substring(2,pageView.length))
+console.log('pageInfo.currPagelist() : ',strPge.substring(2, strPge.length))
