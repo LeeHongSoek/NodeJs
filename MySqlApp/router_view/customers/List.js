a31c4a811fd2f73ea7d7439f691c5d2eca34afce
이홍석(GMail) <lhs0806@gmail.com>
2022-05-31 15:16:28 +0900

    dsf

--- MySqlApp/router_view/customers/List.js
+++ MySqlApp/router_view/customers/List.js
@@ -1,5 +1,5 @@
 const express = require('express')
-const router = express.Router({mergeParams: true}) // https://velog.io/@nittre/Node.jsExpress-라우터에-req.params-값-넘기기
+const router = express.Router() // {mergeParams: true} https://velog.io/@nittre/Node.jsExpress-라우터에-req.params-값-넘기기
 const tableInfo = require('../../router_data/customers')
 
 router.get('/', (req, res) => {
