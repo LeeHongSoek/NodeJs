84b362ba263680b2048963ebc8b0852c9b2b1eea
LeeHongSoek <lhs0806@gmail.com>
2024-12-07 21:15:34 +0900

    Revert "// npm install morgan --save :   추가"
    This reverts commit 2ed1aa1fb700e178a25a46541514c91a795cf048.

--- RestfulAPITDD/lec_06_app.js
+++ RestfulAPITDD/lec_06_app.js
@@ -1,5 +1,5 @@
 const express = require('express')
-const logger = require('morgan') // npm install morgan --save
+const logger = require('morgan')
 const bodyParser = require('body-parser')
 const app = express()
 
