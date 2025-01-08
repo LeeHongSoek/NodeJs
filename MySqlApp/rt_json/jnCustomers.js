d806a3dd1bd465aeb8e3b473cca4b01410b55133
이홍석 <lhs0806@gmail.com>
2025-01-08 09:04:07 +0900

    소스정리

--- MySqlApp/rt_json/jnCustomers.js
+++ MySqlApp/rt_json/jnCustomers.js
@@ -2,7 +2,7 @@ const express = require('express')
 const router = express.Router({mergeParams: true}) // https://velog.io/@nittre/Node.jsExpress-라우터에-req.params-값-넘기기
 const pool = require('../_MySqlDatabase/connection')                                 
 const customersInfo = require('../rt_info/ifCustomers')
-const pageInfo = require('../rt_info/ifPageInfo')
+const pageInfo = require('../rt_info/ifPage')
 
 // 사용예 : >curl -X GET localhost:3000/json/customers/?
 // 사용예 : >curl -X DELETE localhost:3000/json/customers/?
