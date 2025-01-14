41936f00d585756f8416d8b4ed43be15d3076c5a
이홍석 <lhs0806@gmail.com>
2025-01-15 00:14:59 +0900

    maxlength

--- MySqlApp/rt_info/ifOffices.js
+++ MySqlApp/rt_info/ifOffices.js
@@ -6,16 +6,18 @@ const getTotalRowSql = require('./getTotalRowSql')
      tableNameKor : '',                             
                                                       
   fields :  {                                         
- officeCode : { pk:true,     nameKor: '', isListView: true, maxLength:  }  ,
- city : { pk:false,     nameKor: '', isListView: true, maxLength:  }  ,
- phone : { pk:false,     nameKor: '', isListView: true, maxLength:  }  ,
- addressLine1 : { pk:false,     nameKor: '', isListView: true, maxLength:  }  ,
- addressLine2 : { pk:false,     nameKor: '', isListView: true, maxLength:  }  ,
- state : { pk:false,     nameKor: '', isListView: true, maxLength:  }  ,
- country : { pk:false,     nameKor: '', isListView: true, maxLength:  }  ,
- postalCode : { pk:false,     nameKor: '', isListView: true, maxLength:  }  ,
- territory : { pk:false,     nameKor: '', isListView: true, maxLength:  }  ,
+ officeCode : { pk:true,     nameKor: '', isListView: true, maxLength: 10 }  ,
+ city : { pk:false,     nameKor: '', isListView: true, maxLength: 50 }  ,
+ phone : { pk:false,     nameKor: '', isListView: true, maxLength: 50 }  ,
+ addressLine1 : { pk:false,     nameKor: '', isListView: true, maxLength: 50 }  ,
+ addressLine2 : { pk:false,     nameKor: '', isListView: true, maxLength: 50 }  ,
+ state : { pk:false,     nameKor: '', isListView: true, maxLength: 50 }  ,
+ country : { pk:false,     nameKor: '', isListView: true, maxLength: 50 }  ,
+ postalCode : { pk:false,     nameKor: '', isListView: true, maxLength: 15 }  ,
+ territory : { pk:false,     nameKor: '', isListView: true, maxLength: 10 }  ,
             },                                        
+                                                      
+getTotalRowSql, // 전체 레코드수를 구하는 쿼리          
                                                       
  }                                                    
                                                       
