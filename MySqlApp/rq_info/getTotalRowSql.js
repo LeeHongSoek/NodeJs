4748fc6ddaecb37240872eb0ffa155a6fe11ee62
이홍석 <lhs0806@gmail.com>
2025-01-24 14:34:03 +0900

    소스정리

--- MySqlApp/rq_info/getTotalRowSql.js
+++ MySqlApp/rq_info/getTotalRowSql.js
@@ -1,6 +1,7 @@
 // 전체 레코드수를 구하는 쿼리를 만드는 함수는 공통으로 뽑았다..
 getTotalRowSql = (selectSqlKeys) => {
-    return `      /* getTotalRowSql */
+    return ` 
+             /* getTotalRowSql */
              SELECT count(*) AS total_row 
                FROM ( ${selectSqlKeys}  
                     ) A 
